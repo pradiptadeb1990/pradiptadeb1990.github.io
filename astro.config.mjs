a9d40f5052bc45ca87d4cde6c6f6fc9498e030f7
Pradipta Deb <deb.pradipta@gmail.com>
2025-03-21 10:45:37 +0100

    Moving to astro for new website

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import icon from 'astro-icon';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://praditpadeb1990.github.io',
+  integrations: [icon()]
+});
